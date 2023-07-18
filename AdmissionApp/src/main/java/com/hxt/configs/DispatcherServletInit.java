267c6b14e562d0424089a09f4d5a7ffa39061c10
vutrongthang <100742325+vutrongthang@users.noreply.github.com>
2023-07-19 01:44:14 +0700

    update

--- AdmissionApp/src/main/java/com/hxt/configs/DispatcherServletInit.java
+++ AdmissionApp/src/main/java/com/hxt/configs/DispatcherServletInit.java
@@ -24,7 +24,7 @@ public class DispatcherServletInit extends AbstractAnnotationConfigDispatcherSer
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class[] {
-            WebAppContextConfig.class
+            WebApplicationContextConfig.class
         };
     }
 
